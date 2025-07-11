7210d22a02c911cd113fb045104092bd00e401e1
isDohyeon <eun041212@gmail.com>
2025-07-11 19:53:50 +0900

    feat: week1-17 문제 해결

---
+++ week1/17_2675_문자열반복.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+output = []
+for _ in range(t):
+    r, s = input().split()
+    output.append("".join([x * int(r) for x in s]))
+
+sys.stdout.write("\n".join(output))
