01c6b226ce095e0749db9eef1a37885c11ec6c14
isDohyeon <eun041212@gmail.com>
2025-07-12 20:01:53 +0900

    feat: week1-31 문제 해결

---
+++ week1/31_10819_차이를최대로.py
@@ -0,0 +1,36 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = [int(n) for n in input().split()]
+visited = [False] * n
+
+cur_max = 0
+
+
+def calculateSum(cur_arr):
+    sum = 0
+    for i in range(n-1):
+        sum += abs(cur_arr[i] - cur_arr[i + 1])
+    return sum
+
+
+def backtracking(cur_arr: list):
+    if (len(cur_arr) == n):
+        new_max = calculateSum(cur_arr)
+        global cur_max
+        cur_max = max(new_max, cur_max)
+        return
+
+    for i in range(n):
+        if visited[i]:
+            continue
+        visited[i] = True
+        cur_arr.append(arr[i])
+        backtracking(cur_arr)
+        visited[i] = False
+        cur_arr.pop()
+
+
+backtracking([])
+print(cur_max)
