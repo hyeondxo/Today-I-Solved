3a7626bd0d24460a33f01d50352597a146c09e9e
isDohyeon <eun041212@gmail.com>
2025-07-19 09:57:17 +0900

    feat: week2-19 문제 해결

---
+++ week2/19_2812_크게만들기.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+arr = [int(n) for n in input().strip()]
+
+stack = [arr[0]]
+x = []
+result = []
+
+# if k == 1:
+#     print(s)
+
+for i in range(1, n):
+    if arr[i] > stack[-1]:
+        while stack[-1] < arr[i]:
+            x.append(stack.pop())
+            if not stack:
+                break
+            if len(x) == k:
+                for j in stack:
+                    result.append(j)
+                for j in range(i, n):
+                    result.append(arr[j])
+                for num in result:
+                    print(num, end="")
+                sys.exit()
+        stack.append(arr[i])
+    else:
+        stack.append(arr[i])
+
+for i in range(0, n-k):
+    print(stack[i], end="")
